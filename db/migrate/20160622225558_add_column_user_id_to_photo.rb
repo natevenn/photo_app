d47ba639eb038244270c678071aa5f75e5e71932
Nathan Venn <natevenn@gmail.com>
2016-06-22 17:00:59 -0600

    add user id to photos

---
+++ db/migrate/20160622225558_add_column_user_id_to_photo.rb
@@ -0,0 +1,5 @@
+class AddColumnUserIdToPhoto < ActiveRecord::Migration
+  def change
+    add_reference :photos, :user, index: true, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160622224652) do
+ActiveRecord::Schema.define(version: 20160622225558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "photos", force: :cascade do |t|
     t.string   "title"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.integer  "user_id"
   end
+
+  add_index "photos", ["user_id"], name: "index_photos_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -34,4 +37,5 @@ ActiveRecord::Schema.define(version: 20160622224652) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "photos", "users"
 end
